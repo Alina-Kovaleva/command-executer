4e21b4e142e9d3b56e6c6b22ebb02f596d40e218
Alina Kovaleva <calipco.xp@gmail.com>
2024-12-13 11:31:05 +0100

    feat: add input handling with prompt service
    - Implemented the `PromptService` class with support for input, number,
    and password types using Inquirer
    - Integrated the new `PromptService` for handling user input
    
    Implemented functionality to prompt the user for input and
    log the result.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
+import { PromptService } from './core/prompt/prompt.service'
+
 export class App {
-  run() {
-    console.log('Done')
+  async run() {
+    const result = await new PromptService().input<number>('Enter a number', 'number')
+
+    console.log(result)
   }
 }
 
